f6822d1b696ad9f70aba9d897db4d4e8451a2923
Imvikram99 <120546792+Imvikram99@users.noreply.github.com>
2024-01-20 15:44:22 +0530

    port

--- src/test/java/dev/apipulse/brbhr/BrbhrApplicationTests.java
+++ src/test/java/dev/apipulse/brbhr/BrbhrApplicationTests.java
@@ -3,7 +3,7 @@ package dev.apipulse.brbhr;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BrbhrApplicationTests {
 
 	@Test
