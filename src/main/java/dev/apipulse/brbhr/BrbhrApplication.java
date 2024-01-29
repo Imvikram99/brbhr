3f553e21306c8daaa9cd1eb306af95be7e0b109c
Imvikram99 <120546792+Imvikram99@users.noreply.github.com>
2024-01-29 17:51:06 +0530

    added apipulse

--- src/main/java/dev/apipulse/brbhr/BrbhrApplication.java
+++ src/main/java/dev/apipulse/brbhr/BrbhrApplication.java
@@ -1,9 +1,14 @@
 package dev.apipulse.brbhr;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ai.apiverse.apisuite.mirror.agent.ApimonitorAutoConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
+@ImportAutoConfiguration(ApimonitorAutoConfiguration.class)
 public class BrbhrApplication {
 
 	public static void main(String[] args) {
